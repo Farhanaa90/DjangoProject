00fc1deeeaa65061000f5eb824bf832f07496f8f
Farhana Chowdhury <farhanapushpo2002@gmail.com>
2025-10-12 01:14:35 +0600

    Created models.py, base,html and profile.html

--- Share4Care/users/admin.py
+++ Share4Care/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserProfile, Donation, DonationClaim, ContactMessage
 
-# Register your models here.
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status', 'donor', 'city', 'created_at']
+    list_filter = ['category', 'status', 'city']
+
+class ClaimAdmin(admin.ModelAdmin):
+    list_display = ['donation', 'collector', 'status', 'claimed_at']
+    list_filter = ['status']
+
+admin.site.register(UserProfile)
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(DonationClaim, ClaimAdmin)
+admin.site.register(ContactMessage)
